90ca6a714e3a49b02d3dc7586ea82d8f4ae2ae01
Iara Vaneti <iara.vaneti@ifood.com.br>
2025-03-05 10:25:42 -0300

    Finalizing repositories, mappers, use cases and view model unit tests

--- app/src/test/java/com/example/rickandmortyapp/characterDatails/domain/usecase/GetCharacterByIdUseCaseTest.kt
+++ app/src/test/java/com/example/rickandmortyapp/characterDatails/domain/usecase/GetCharacterByIdUseCaseTest.kt
@@ -1,15 +1,18 @@
 package com.example.rickandmortyapp.characterDatails.domain.usecase
 
+import android.util.Log
 import com.example.rickandmortyapp.characterDatails.domain.model.CharacterDetails
 import com.example.rickandmortyapp.common.domain.repository.CharacterRepository
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
+import io.mockk.mockkStatic
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -38,5 +41,39 @@ class GetCharacterByIdUseCaseTest {
 
         coVerify { characterRepository.getCharacterById(id) }
         assert(result == expectedCharactersList)
+    }
+
+    @Test
+    fun `GIVEN get character by id WHEN repository throws exception THEN throw exception`() = runTest {
+        val id = 1
+        val repositoryException = Exception("Error")
+        val expectedException = Exception("Erro ao buscar detalhes do personagem: ${repositoryException.message}")
+
+        mockkStatic(Log::class)
+        coEvery { Log.e(any(), any()) } returns 0
+
+        coEvery { characterRepository.getCharacterById(id) } throws repositoryException
+
+        val result = runCatching {
+            getCharacterByIdUseCase(id).first()
+        }
+
+        assertTrue(result.exceptionOrNull()?.message == expectedException.message)
+        assert(result.exceptionOrNull() is Exception)
+    }
+
+    @Test
+    fun `GIVEN get character by id WHEN called with invalid id THEN throw exception`() = runTest {
+        val id = 0
+
+        mockkStatic(Log::class)
+        coEvery { Log.e(any(), any()) } returns 0
+
+        val result = runCatching {
+            getCharacterByIdUseCase(id).first()
+        }
+
+        assertTrue(result.exceptionOrNull() is IllegalArgumentException)
+        assertTrue(result.exceptionOrNull()?.message == "Invalid character id")
     }
 }
